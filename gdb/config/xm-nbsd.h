ed20a4786f2177ddb10716ce386b41eae69696e4
Jason Molenda <jsm@bugshack.cygnus.com>
1999-12-14 01:06:04 +0000

    import gdb-1999-12-13 snapshot

--- gdb/config/xm-nbsd.h
+++ gdb/config/xm-nbsd.h
@@ -1,5 +1,5 @@
 /* Host-dependent definitions for any CPU running NetBSD.
-   Copyright 1993, 1994, 1996 Free Software Foundation, Inc.
+   Copyright 1993, 1994, 1996, 1999 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
@@ -17,6 +17,10 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
+
+/* Include this to get things like NGROUPS which <limits.h> doesn't
+   define on some systems. */
+#include <sys/param.h>
 
 /* We have to include these files now, so that GDB will not make
    competing definitions in defs.h.  */
