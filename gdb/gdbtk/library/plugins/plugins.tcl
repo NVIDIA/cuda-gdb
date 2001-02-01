ddff8c6bfc7d02b08995fd1e7a0ccd39edc0134b
Fernando Nasser <fnasser@redhat.com>
2001-02-01 21:03:43 +0000

    2001-02-01  Fernando Nasser  <fnasser@redhat.com>
    	* library/gdbmenubar.itcl (constructor): Remove unused code.
    	(add): New method.  Emulate the style of the menubar iwidget commands.
    	(menubar_show): Old name.
    	(show): New name.
    	(menubar_menu_exists): Old name.
    	(exists): New name.
    	(menubar_clear_menu): Old name.
    	(clear): New name.
    	(menubar_delete_menu): Old name.
            (delete): New name.
    	(menubar_new_menu): Make private.  Does not return anything.
    	(menubar_add_menu_command): Make private.
    	(menubar_add_menu_separator): Ditto.
    	(menubar_change_menu_state): Ditto.
    	* library/srcbar.itcl: Adjust to the above syntax.
    	* library/plugins/plugins.tcl: Ditto.
    	* library/plugins/rhabout/rhabout.itcl: Ditto.

--- gdb/gdbtk/library/plugins/plugins.tcl
+++ gdb/gdbtk/library/plugins/plugins.tcl
@@ -5,9 +5,9 @@ if {1} {  #test here if your target is configured
   # Uncomment this when the PlugIn class is ready
   #package require PLUGIN 1.0
   package require RHABOUT 1.0
-  $Menu menubar_add_menu_command Other "About Red Hat" \
+  $Menu add command Other "About Red Hat" \
     {ManagedWin::open RHAbout -transient} \
     -underline 0
   # To activate the PlugIn sample, uncomment the next line
-  #set plugins_available 1
+  set plugins_available 1
 }
