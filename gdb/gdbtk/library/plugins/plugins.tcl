6b1782d7fce979e989bd994784be8c08ce12b3a6
Fernando Nasser <fnasser@redhat.com>
2001-02-05 02:24:55 +0000

    2001-02-04  Fernando Nasser  <fnasser@redhat.com>
    	* library/plugins/plugins.tcl: Fix typos and remove "-transient"
    	parameter from open command.
    	* library/plugins/HOW-TO: New file.  Describes how to add a plug-in
    	window to Insight.

--- gdb/gdbtk/library/plugins/plugins.tcl
+++ gdb/gdbtk/library/plugins/plugins.tcl
@@ -1,12 +1,12 @@
 # Add your window to the PlugIn menu here
-# Dont forget to add your packet as well
+# Don't forget to add your packet as well
 
 if {1} {  #test here if your target is configured
   # Uncomment this when the PlugIn class is ready
-  #package require PLUGIN 1.0
+  #package require LIBGDB 1.0
   package require RHABOUT 1.0
   $Menu add command Other "About Red Hat" \
-    {ManagedWin::open RHAbout -transient} \
+    {ManagedWin::open RHAbout} \
     -underline 0
   # To activate the PlugIn sample, uncomment the next line
   set plugins_available 1
