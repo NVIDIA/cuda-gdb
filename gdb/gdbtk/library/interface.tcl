172eaaa8b4c0069779769b5fd6aadfa64bceeeaf
Keith Seitz <keiths@redhat.com>
2002-01-03 00:03:46 +0000

    Update copyright.

--- gdb/gdbtk/library/interface.tcl
+++ gdb/gdbtk/library/interface.tcl
@@ -1,5 +1,5 @@
 # Interface between GDB and Insight.
-# Copyright 1997, 1998, 1999, 2001 Red Hat, Inc.
+# Copyright 1997, 1998, 1999, 2001, 2002 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License (GPL) as published by
